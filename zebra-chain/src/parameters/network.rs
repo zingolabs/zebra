142266653c31aeb62e4b3c22dc2f90926ab96fdb
idky137 <idky137@gmail.com>
2024-03-08 16:19:28 +0000

    removed derive(copy) from network

--- zebra-chain/src/parameters/network.rs
+++ zebra-chain/src/parameters/network.rs
@@ -52,7 +52,7 @@ mod tests;
 const ZIP_212_GRACE_PERIOD_DURATION: HeightDiff = 32_256;
 
 /// An enum describing the possible network choices.
-#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[derive(Clone, Debug, Default, Eq, PartialEq, Hash, Serialize, Deserialize)]
 #[cfg_attr(any(test, feature = "proptest-impl"), derive(Arbitrary))]
 pub enum Network {
     /// The production mainnet.
@@ -64,6 +64,7 @@ pub enum Network {
 }
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[cfg_attr(any(test, feature = "proptest-impl"), derive(Arbitrary))]
 pub struct NetworkParameters {
     // TODO: Add testnet network parameters here.
 }
